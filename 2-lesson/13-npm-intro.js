9bd15327024cd32e0b6ebdcf7821dc50586c309e
Minol Hewage <minolmalshan01@gmail.com>
2022-11-05 12:07:26 +0530

    node_modules, package.json created.

--- 2-lesson/13-npm-intro.js
+++ 2-lesson/13-npm-intro.js
@@ -13,9 +13,13 @@
 // npm init (step-by-step, press enter to skip)
 //npm init -y (everything is default)
 
+//dev dependency
+//npm install <package name> --save-dev
 const __ = require("lodash");
 
 const items = [1,[2,[3,[4]]]]
 const newItems = __.flattenDeep(items)
 
 console.log(newItems)
+
+
