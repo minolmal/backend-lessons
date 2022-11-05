18d5251d74d8cb4f1335e4df62c6f1d3bb99c5b2
Minol Hewage <minolmalshan01@gmail.com>
2022-11-05 11:26:21 +0530

    node modules + package.json added

---
+++ 2-lesson/13-npm-intro.js
@@ -0,0 +1,21 @@
+// npm -global command, comes with node
+// npm --version, npm --v
+
+//local dependency - use it only in your project
+//npm i <package name>
+
+//global dependency - use it in any project
+//npm install -g <package name>
+//sudo npm install -g <package name>
+
+//package.json - manifest file (stores important info about project/package)
+//manual approach (create package.json in the root, create properties etc)
+// npm init (step-by-step, press enter to skip)
+//npm init -y (everything is default)
+
+const __ = require("lodash");
+
+const items = [1,[2,[3,[4]]]]
+const newItems = __.flattenDeep(items)
+
+console.log(newItems)
